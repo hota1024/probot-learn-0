fbc0d6764c2eb23e050a01856c1cc1aa52575149
hota1024 <hotalog@hota1024.com>
2020-04-29 00:14:19 +0900

    Update

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,12 @@
 import { Application } from 'probot' // eslint-disable-line no-unused-vars
 
 export = (app: Application) => {
-  app.on('issues.opened', async (context) => {
-    const issueComment = context.issue({ body: 'Thanks for opening this issue!' })
-    await context.github.issues.createComment(issueComment)
+  // app.on('issues.opened', async (context) => {
+  //   const issueComment = context.issue({ body: 'Thanks for opening this issue!' })
+  //   await context.github.issues.createComment(issueComment)
+  // })
+  app.on('push', async (context) => {
+    console.log(context)
   })
   // For more information on building apps:
   // https://probot.github.io/docs/
